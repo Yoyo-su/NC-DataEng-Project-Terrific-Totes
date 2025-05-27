34d93cc9f2a6706fb659cdf273995654e3f9e851
sukansasi <sukanyasasi2013@gmail.com>
2025-05-27 14:53:11 +0100

    checking

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,9 +1,8 @@
-import pytest # type: ignore
+import pytest  # type: ignore
 from src.sample import sample
 
 
 @pytest.mark.it("Sample test")
 def test_sample():
     result = sample()
-
     assert result == "hello"
