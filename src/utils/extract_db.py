5d49d16b194a066d4e7662f7221c22526ff2d5ee
Yoyo-su <iohannan211@gmail.com>
2025-05-28 14:12:18 +0100

    reformatted extract files

--- src/utils/extract_db.py
+++ src/utils/extract_db.py
@@ -1,6 +1,16 @@
 from db.connection import connect_to_db, close_db
 
+
 def extract_db(table_name):
+    """Lambda function that extracts data from a database table and returns it as a formatted dictionary
+
+    Args:
+        table_name (string): table to extract data from
+
+    Returns:
+        _type_: _description_
+    """
+
     try:
         conn = connect_to_db(".env.totesys")
         query = f"SELECT * FROM {table_name}"
@@ -8,10 +18,10 @@ def extract_db(table_name):
         columns = [column["name"] for column in conn.columns]
         table_dict = {table_name: [dict(zip(columns, row)) for row in response]}
         return table_dict
+
     except Exception as error:
         print(f"Failed to extract from DB: {error}")
+
     finally:
         if conn:
             close_db(conn)
-
-        
--- tests/test_extract_db.py
+++ tests/test_extract_db.py
@@ -5,10 +5,13 @@ import datetime
 
 """Tests for the extract_db util function"""
 
+
 class TestExtractDB:
     @pytest.mark.it("Testing returns dictionary with expected key and value as list")
     @patch("src.utils.extract_db.connect_to_db")
-    def test_extract_db_returns_dictionary_with_expected_key_and_list_value(self,mock_connect_to_db):
+    def test_extract_db_returns_dictionary_with_expected_key_and_list_value(
+        self, mock_connect_to_db
+    ):
         mock_conn = Mock()
         mock_conn.run.return_value = []
         mock_conn.columns = []
@@ -16,30 +19,134 @@ class TestExtractDB:
         result = extract_db("staff")
         assert isinstance(result, dict)
         assert "staff" in result.keys()
-        assert isinstance(result["staff"],list)
+        assert isinstance(result["staff"], list)
 
-    @pytest.mark.it("Testing extract db with empty table returns dictionary with empty list")
+    @pytest.mark.it(
+        "Testing extract db with empty table returns dictionary with empty list"
+    )
     @patch("src.utils.extract_db.connect_to_db")
-    def test_extract_db_returns_empty_list_inside_dictionary(self,mock_connect_to_db):
+    def test_extract_db_returns_empty_list_inside_dictionary(self, mock_connect_to_db):
         mock_conn = Mock()
         mock_conn.run.return_value = []
         mock_conn.columns = []
         mock_connect_to_db.return_value = mock_conn
-        expected = {"staff":[]}
+        expected = {"staff": []}
         result = extract_db("staff")
         assert expected == result
 
     @pytest.mark.it("Testing extract db returns data as formatted dictionary")
     @patch("src.utils.extract_db.connect_to_db")
-    def test_extract_db_returns_expected_values(self,mock_connect_to_db):
+    def test_extract_db_returns_expected_values(self, mock_connect_to_db):
         mock_conn = Mock()
-        mock_conn.run.return_value = [[1, 'person', 'one', 2, 'person1@terrifictotes.com', datetime.datetime(2022, 11, 3, 14, 20, 51, 563000), 
-                                    datetime.datetime(2022, 11, 3, 14, 20, 51, 563000)],
-                                    [2, 'person', 'two', 6, 'person2@terrifictotes.com', datetime.datetime(2022, 11, 3, 14, 20, 51, 563000), datetime.datetime(2022, 11, 3, 14, 20, 51, 563000)]]
+        mock_conn.run.return_value = [
+            [
+                1,
+                "person",
+                "one",
+                2,
+                "person1@terrifictotes.com",
+                datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),
+                datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),
+            ],
+            [
+                2,
+                "person",
+                "two",
+                6,
+                "person2@terrifictotes.com",
+                datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),
+                datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),
+            ],
+        ]
 
-        mock_conn.columns = [{'table_oid': 16466, 'column_attrnum': 1, 'type_oid': 23, 'type_size': 4, 'type_modifier': -1, 'format': 0, 'name': 'staff_id'}, {'table_oid': 16466, 'column_attrnum': 2, 'type_oid': 25, 'type_size': -1, 'type_modifier': -1, 'format': 0, 'name': 'first_name'}, {'table_oid': 16466, 'column_attrnum': 3, 'type_oid': 25, 'type_size': -1, 'type_modifier': -1, 'format': 0, 'name': 'last_name'}, {'table_oid': 16466, 'column_attrnum': 4, 'type_oid': 23, 'type_size': 4, 'type_modifier': -1, 'format': 0, 'name': 'department_id'}, {'table_oid': 16466, 'column_attrnum': 5, 'type_oid': 25, 'type_size': -1, 'type_modifier': -1, 'format': 0, 'name': 'email_address'}, {'table_oid': 16466, 'column_attrnum': 6, 'type_oid': 1114, 'type_size': 8, 'type_modifier': 3, 'format': 0, 'name': 'created_at'}, {'table_oid': 16466, 'column_attrnum': 7, 'type_oid': 1114, 'type_size': 8, 'type_modifier': 3, 'format': 0, 'name': 'last_updated'}]
+        mock_conn.columns = [
+            {
+                "table_oid": 16466,
+                "column_attrnum": 1,
+                "type_oid": 23,
+                "type_size": 4,
+                "type_modifier": -1,
+                "format": 0,
+                "name": "staff_id",
+            },
+            {
+                "table_oid": 16466,
+                "column_attrnum": 2,
+                "type_oid": 25,
+                "type_size": -1,
+                "type_modifier": -1,
+                "format": 0,
+                "name": "first_name",
+            },
+            {
+                "table_oid": 16466,
+                "column_attrnum": 3,
+                "type_oid": 25,
+                "type_size": -1,
+                "type_modifier": -1,
+                "format": 0,
+                "name": "last_name",
+            },
+            {
+                "table_oid": 16466,
+                "column_attrnum": 4,
+                "type_oid": 23,
+                "type_size": 4,
+                "type_modifier": -1,
+                "format": 0,
+                "name": "department_id",
+            },
+            {
+                "table_oid": 16466,
+                "column_attrnum": 5,
+                "type_oid": 25,
+                "type_size": -1,
+                "type_modifier": -1,
+                "format": 0,
+                "name": "email_address",
+            },
+            {
+                "table_oid": 16466,
+                "column_attrnum": 6,
+                "type_oid": 1114,
+                "type_size": 8,
+                "type_modifier": 3,
+                "format": 0,
+                "name": "created_at",
+            },
+            {
+                "table_oid": 16466,
+                "column_attrnum": 7,
+                "type_oid": 1114,
+                "type_size": 8,
+                "type_modifier": 3,
+                "format": 0,
+                "name": "last_updated",
+            },
+        ]
         mock_connect_to_db.return_value = mock_conn
 
-        expected = {"staff":[{'staff_id': 1, 'first_name': 'person', 'last_name': 'one', 'department_id': 2, 'email_address': 'person1@terrifictotes.com', 'created_at': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000), 'last_updated': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000)}, {'staff_id': 2, 'first_name': 'person', 'last_name': 'two', 'department_id': 6, 'email_address': 'person2@terrifictotes.com', 'created_at': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000), 'last_updated': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000)}]}
+        expected = {
+            "staff": [
+                {
+                    "staff_id": 1,
+                    "first_name": "person",
+                    "last_name": "one",
+                    "department_id": 2,
+                    "email_address": "person1@terrifictotes.com",
+                    "created_at": datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),
+                    "last_updated": datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),
+                },
+                {
+                    "staff_id": 2,
+                    "first_name": "person",
+                    "last_name": "two",
+                    "department_id": 6,
+                    "email_address": "person2@terrifictotes.com",
+                    "created_at": datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),
+                    "last_updated": datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),
+                },
+            ]
+        }
         result = extract_db("staff")
         assert expected == result
