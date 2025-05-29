32fb5572eddc53a81bdc6378186ff10252dd93e3
Yoyo-su <iohannan211@gmail.com>
2025-05-29 12:55:01 +0100

    print key instead of file contents

--- src/utils/insert_into_s3.py
+++ src/utils/insert_into_s3.py
@@ -4,7 +4,7 @@ from botocore.exceptions import ClientError
 def upload_json_to_s3(json_file, bucket_name, key, s3_client):
     try:
         s3_client.put_object(Body=json_file, Bucket=bucket_name, Key=key)
-        print(f"Successfully uploaded {json_file} to s3://{bucket_name}/{key}")
+        print(f"Successfully uploaded {key} to s3://{bucket_name}/{key}")
 
     except ClientError as e:
         print(f"Failed to put object: {e}")
