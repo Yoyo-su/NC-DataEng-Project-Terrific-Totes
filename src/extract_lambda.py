f0391bb1d59c87a40b5e55d4a0673914d674a936
Yoyo-su <iohannan211@gmail.com>
2025-05-29 12:35:16 +0100

    Extract lambda now runs full extraction once, all following iterations collect only new data

--- src/extract_lambda.py
+++ src/extract_lambda.py
@@ -1,7 +1,7 @@
 import boto3
-from utils.extract_db import extract_db
-from utils.json_dumps import dump_to_json
-from utils.insert_into_s3 import upload_json_to_s3
+from src.utils.extract_db import extract_db
+from src.utils.json_dumps import dump_to_json
+from src.utils.insert_into_s3 import upload_json_to_s3
 from datetime import datetime
 
 
@@ -31,15 +31,23 @@ def lambda_handler(event, context):
         ]
 
         for table in table_list:
-            table_dict = extract_db(table)
+            try:
+                """Run query with last updated if last_updated.txt exists"""
+                response = s3_client.get_object(
+                    Bucket="fscifa-raw-data", Key="last_updated.txt"
+                )
+                last_updated = response["Body"].read().decode("utf-8")
+                check_time = last_updated.replace("T", " ")
+                table_dict = extract_db(table, check_time)
+            except Exception:
+                """Run full select * query if not"""
+                table_dict = extract_db(table)
             key = f"{table}/{table}-{timestamp}.json"
-            json_data = dump_to_json(table_dict)
-            upload_json_to_s3(json_data, "fscifa-raw-data", key, s3_client)
-
+            if table_dict[table]:
+                json_data = dump_to_json(table_dict)
+                upload_json_to_s3(json_data, "fscifa-raw-data", key, s3_client)
+        upload_json_to_s3(timestamp, "fscifa-raw-data", "last_updated.txt", s3_client)
         return {"result": "success"}
     except Exception as error:
         print(f"Failed to extract data from database: {error}")
         raise Exception
-
-
-lambda_handler({},{})
--- src/utils/extract_db.py
+++ src/utils/extract_db.py
@@ -1,7 +1,7 @@
 from db.connection import connect_to_db, close_db
 
 
-def extract_db(table_name):
+def extract_db(table_name, last_updated=None):
     """Lambda function that extracts data from a database table and returns it as a formatted dictionary
 
     Args:
@@ -13,7 +13,10 @@ def extract_db(table_name):
 
     try:
         conn = connect_to_db(".env.dev")
-        query = f"SELECT * FROM {table_name}"
+        if not last_updated:
+            query = f"SELECT * FROM {table_name};"
+        else:
+            query = f"SELECT * FROM {table_name} WHERE last_updated > '{last_updated}';"
         response = conn.run(query)
         columns = [column["name"] for column in conn.columns]
         table_dict = {table_name: [dict(zip(columns, row)) for row in response]}
