1888ade36f8881ccc7d0ebc468dde31b8bfd584a
Yoyo-su <iohannan211@gmail.com>
2025-05-29 12:44:05 +0100

    Delete sample files

--- src/sample.py
+++
@@ -1,3 +0,0 @@
-def sample():
-    print("hello world")
-    return "hello"
--- tests/test_sample.py
+++
@@ -1,8 +0,0 @@
-import pytest  # type: ignore
-from src.sample import sample
-
-
-@pytest.mark.it("Sample test")
-def test_sample():
-    result = sample()
-    assert result == "hello"
