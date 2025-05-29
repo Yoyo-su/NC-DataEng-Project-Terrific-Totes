05ce05a31ccb52defcb97c286c244d7064ffe9aa
Freddie Moller <frederickfrmoller@gmail.com>
2025-05-29 12:01:16 +0100

    woahhh added new tests and such

--- src/utils/insert_into_s3.py
+++ src/utils/insert_into_s3.py
@@ -3,9 +3,21 @@ import logging
 
 
 def upload_json_to_s3(json_file, bucket_name, key, s3_client):
+    """
+    Uploads a JSON string to an s3 bucket
+
+    Args:
+        - json_file (json str): a json-formatted string
+        - bucket_name (str): the name of the bucket you're putting the JSON data into
+        - key (str): the location inside the s3 bucket that you want to put the data into
+        - s3_client
+
+    Output:
+        - Places the json data in the specified key in the specified bucket
+    """
     try:
-        s3_client.put_object(Body = json_file,Bucket = bucket_name,Key = key)
-        print(f"Successfully uploaded {json_file} to s3://{bucket_name}/{key}")    
+        s3_client.put_object(Body=json_file, Bucket=bucket_name, Key=key)
+        print(f"Successfully uploaded {json_file} to s3://{bucket_name}/{key}")
     except ClientError as e:
         logging.error(e)
-        # raise e
+        raise e
--- src/utils/json_dumps.py
+++ src/utils/json_dumps.py
@@ -1,4 +1,13 @@
 import json
 
 def dump_to_json(data):
+    """
+    Util function that takes in a dictionary and converts it to json format for storage
+    Args:
+        data (dictionary): a dictionary representing data from a table
+    Returns:
+        a json file
+    """
     return json.dumps(data, default=str) 
+
+print(type(dump_to_json({'hello':'oops'})))
