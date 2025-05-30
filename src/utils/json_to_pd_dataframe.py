07ff83fc236a842092eaa4955f01a9ac8a0bc694
CharlotteC63 <ccampbell6066@gmail.com>
2025-05-30 16:27:22 +0100

    refactoring function and tests, add docstrings and reformat

--- src/utils/json_to_pd_dataframe.py
+++ src/utils/json_to_pd_dataframe.py
@@ -1,49 +1,41 @@
 import pandas as pd
 import json
+import boto3
+from botocore.exceptions import ClientError
 
-def json_to_pd_dataframe(most_recent_file: str, table_name):
-    if most_recent_file:
-        with open(most_recent_file,'r') as file:
+
+def json_to_pd_dataframe(most_recent_file: str, table_name, bucket_name):
+    """
+        This function:
+               - downloads most_recent_file containing table_name in its name, from specified s3 bucket
+               - loads the data from this json file into a pandas dataframe, which is returned
+
+    Arguments: - most_recent_file, which is the most recent file in the s3 bucket, "fscifa-raw-data", with the specified table_name
+               - table_name, which is a table name from the original OLTP database.
+               - bucket_name, which should be set to "fscifa-raw-data" in production.
+
+    Returns: a pandas dataframe of the data from the specified file.
+
+    """
+    try:
+        resource = boto3.resource("s3")
+        bucket = resource.Bucket(bucket_name)
+        bucket.download_file(most_recent_file, most_recent_file)
+        with open(most_recent_file, "r") as file:
             data = json.load(file)
             result = pd.DataFrame(data=data[table_name])
         print(result)
         return result
-    else:
-        raise Exception("Table is not found")
-    
-
-
-
-
-
-
-
-
-# def read_file_and_turn_it_to_list_dict(table_name, bucket_name, most_recent_file):
-#     resource = boto3.resource("s3")
-#     bucket = resource.Bucket(bucket_name)
-#     try:
-#         bucket.download_file(most_recent_file, most_recent_file)
-#         with open(most_recent_file, "r") as file:
-#             data = json.load(file)[table_name]
-#             data_records = [item for item in data]
-#         return data_records
-#     except ValueError:
-#         return [{}]
-#     except ClientError:
-#         return [{}]
-
-
-
-# [{},{}]
-
-# import pandas as pd 
-  
-# # Initialise data to lists. 
-# data = [{'Geeks': 'dataframe', 'For': 'using', 'geeks': 'list'},
-#         {'Geeks':10, 'For': 20, 'geeks': 30}] 
-
-# df = pd.DataFrame.from_dict(data)
-# print(df)
-
-
+    except Exception:
+        if not most_recent_file.startswith(table_name):
+            raise Exception(
+                "Error when converting file to dataframe: incorrect table_name"
+            )
+        elif not most_recent_file.endswith(".json"):
+            raise Exception(
+                "Error when converting file to dataframe: most_recent_file should be of type json"
+            )
+        else:
+            raise ClientError(
+                "Error retrieving data from specified bucket, check bucket_name is correct"
+            )
--- tests/test_json_to_pd_dataframe.py
+++ tests/test_json_to_pd_dataframe.py
@@ -1,39 +1,96 @@
 import pytest
 import pandas as pd
 from src.utils.json_to_pd_dataframe import json_to_pd_dataframe
+from moto import mock_aws
+import boto3
+import os
 
 
-class TestDictTransformedToDataframe:
+@pytest.fixture
+def aws_creds():
+    os.environ["AWS_ACCESS_KEY_ID"] = "Test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "Test"
+    os.environ["AWS_SECURITY_TOKEN"] = "Test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture()
+def s3_resource(aws_creds):
+    with mock_aws():
+        yield boto3.resource("s3", region_name="eu-west-2")
+
+
+@pytest.fixture()
+def bucket(aws_creds, s3_resource):
+    with mock_aws():
+        s3_resource.create_bucket(
+            Bucket="test_ingest_bucket",
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        bucket = s3_resource.Bucket("test_ingest_bucket")
+        return bucket
+
+
+class TestJsonToPDdataframe:
+    @pytest.mark.it("when passed a file not of type json, should raise exception")
+    def test_incorrect_file_type_raises_exception(self, bucket):
+        test_file_1 = "address-2025-06-29T11:06:18.399084.txt"
+        with pytest.raises(
+            Exception,
+            match=r"Error when converting file to dataframe: most_recent_file should be of type json",
+        ):
+            json_to_pd_dataframe(test_file_1, "address", "test_ingest_bucket")
+
+    @pytest.mark.it("when passed incorrect table name, should raise exception")
+    def test_incorrect_table_name_raises_exception(self, bucket):
+        test_file_1 = "address-2025-06-29T11:06:18.399084.txt"
+        with pytest.raises(
+            Exception,
+            match=r"Error when converting file to dataframe: incorrect table_name",
+        ):
+            json_to_pd_dataframe(test_file_1, "payments", "test_ingest_bucket")
+
     @pytest.mark.it(
-        "when passed a json file with one record returns dataframe with one row" 
+        "when passed an incorrect bucket_name, raises ClientError with appropriate message"
     )
-    def test_returns_dataframe_with_one_row(self):
+    def test_incorrect_bucket_name_raises_exception(self, bucket):
+        test_file_1 = "address-2025-06-29T11:06:18.399084.txt"
+        with pytest.raises(
+            Exception,
+            match=r"Error when converting file to dataframe: most_recent_file should be of type json",
+        ):
+            json_to_pd_dataframe(test_file_1, "address", "test_wrong_bucket")
+
+    @pytest.mark.it(
+        "when passed a json file with one record returns dataframe with one row"
+    )
+    def test_returns_dataframe_with_one_row(self, bucket):
         test_file_1 = "address-2025-06-29T11:06:18.399084.json"
         with open(test_file_1, "w") as file:
             file.write(
                 '{"address": [{"address_id": 1, "address_line_1": "6826 Herzog Via"}]}'
             )
-        result = json_to_pd_dataframe(test_file_1,'address') 
-        assert isinstance(result,pd.DataFrame) 
+        bucket.upload_file(test_file_1, test_file_1)
+        result = json_to_pd_dataframe(test_file_1, "address", "test_ingest_bucket")
+        assert isinstance(result, pd.DataFrame)
         assert len(result) == 1
         assert result["address_id"][0] == 1
         assert result["address_line_1"][0] == "6826 Herzog Via"
 
     @pytest.mark.it(
-        "when passed a json file with two records returns dataframe with two rows" 
+        "when passed a json file with two records returns dataframe with two rows"
     )
-    def test_returns_dataframe_with_multiple_rows(self):
+    def test_returns_dataframe_with_multiple_rows(self, bucket):
         test_file_1 = "address-2025-06-29T11:06:18.399084.json"
         with open(test_file_1, "w") as file:
             file.write(
                 '{"address": [{"address_id": 1, "address_line_1": "6826 Herzog Via"},{"address_id": 2, "address_line_1": "93 High Street"}]}'
             )
-        result = json_to_pd_dataframe(test_file_1,'address') 
-        assert isinstance(result,pd.DataFrame) 
+        bucket.upload_file(test_file_1, test_file_1)
+        result = json_to_pd_dataframe(test_file_1, "address", "test_ingest_bucket")
+        assert isinstance(result, pd.DataFrame)
         assert len(result) == 2
         assert result["address_id"][0] == 1
         assert result["address_line_1"][0] == "6826 Herzog Via"
         assert result["address_id"][1] == 2
         assert result["address_line_1"][1] == "93 High Street"
-        
-
