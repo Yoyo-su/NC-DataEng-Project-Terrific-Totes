f609f734c8e82018a307fd14ec7e2728a9fc052f
Freddie Moller <frederickfrmoller@gmail.com>
2025-06-04 12:00:09 +0100

    pseudocode etc

--- src/utils/parquet_to_sql.py
+++ src/utils/parquet_to_sql.py
@@ -1,7 +1,15 @@
 # - read the parquet
 # - create the query from the parquet
 
-import pandas
+import pandas as pd
 import pyarrow
+from db.connection import connect_to_db, close_db
 
-print(pandas.read_parquet('/home/frederick/FSCIFA-project/test_staff.parquet',engine='pyarrow'))
+print(pd.read_parquet('/home/frederick/FSCIFA-project/test_staff.parquet',engine='pyarrow'))
+
+def parquet_to_sql(parquet_filename, parquet_filepath, bucket):
+    df = pd.read_parquet('/home/frederick/FSCIFA-project/test_staff.parquet',engine='pyarrow')
+
+# df = pd.read_parquet(parquet_path)
+# column_name = df.columns
+# INSERT INTO {table_name} ({column_name}) VALUES
