0331bddab0924f0af369cf7e51340a8787d287c3
CharlotteC63 <ccampbell6066@gmail.com>
2025-06-04 15:04:19 +0100

    add docstrings

--- src/utils/transform_dimension_tables.py
+++ src/utils/transform_dimension_tables.py
@@ -14,8 +14,8 @@ def transform_dim_location():
     - check whether this returns a file name string (which will be the case if new data has been added to the address table in the totesys database)
     - if an exception is raised, transform_dim_location returns nothing (because there is no new data to be transformed)
     - otherwise, json_to_pd_dataframe is invoked, which returns a dataframe for new address data, location_df
-    - within location_df, the address_id column is renamed to location_id, to match specification
-    - within location_df, columns, "created_at" and "last_updated" are dropped, to match specification
+    - address_id column of location_df is renamed to location_id, to match specification
+    - columns, "created_at" and "last_updated" are dropped from location_df, to match specification
     - transformed location_df (dataframe) is returned
 
     Arguments: no arguments.
@@ -41,7 +41,7 @@ def transform_dim_counterparty():
     - if an exception is raised, transform_dim_counterparty returns nothing (because there is no new data to be transformed)
     - otherwise, json_to_pd_dataframe is invoked, which returns a dataframe for new counterparty data, counterparty_df
     - columns, "created_at", "last_updated", "delivery_contact", "commercial_contact" dropped from counterparty_df, to match specification
-    - counterparty_df is left merged with location_df (by invoking transform_dim_location), to obtain location data for the counterparty
+    - counterparty_df is left merged with location_df (created by invoking transform_dim_location), to add location data for the counterparty
     - columns, "location_id", "legal_address_id", dropped from counterparty_df, to match specification
     - for loop is used to rename particular columns to match specification
     - transformed counterparty_df (dataframe) is returned
@@ -125,8 +125,20 @@ def transform_dim_currency():
 
 
 def get_department_data():
+    """
+    This function:
+    - calls find_files_with_specified_table_name, which returns a list of json files from department folder of s3 bucket, fscifa-raw-data
+    - assigns this list of json files to variable, files
+    - creates a dataframe (department_df) out of the first json file in the list, by calling json_to_pd_dataframe
+    - loops through remaining json files in list, and creates a dataframe for each (additional_df), and appends these dataframes to department_df
+    - returns department_df
+
+    Arguments: no arguments.
+
+    Returns: a dataframe (department_df) containing all department data to date.
+
+    """
     files = find_files_with_specified_table_name("department", "fscifa-raw-data")
-    print(files)
     department_df = json_to_pd_dataframe(files[0], "department", "fscifa-raw-data")
     for i in range(1, len(files)):
         additional_df = json_to_pd_dataframe(files[i], "department", "fscifa-raw-data")
@@ -135,37 +147,65 @@ def get_department_data():
 
 
 def transform_dim_staff():
+    """
+    This function:
+    - calls find_most_recent_json_filename
+    - check whether this returns a file name string (will be the case if new data has been added to the staff table in the totesys database)
+    - if an exception is raised, transform_dim_staff returns nothing (because there is no new data to be transformed)
+    - otherwise, json_to_pd_dataframe is invoked, which returns a dataframe for new staff data, staff_df
+    - create department_df by invoking get_department_data
+    - columns "manager", "created_at" and "last_updated" are dropped from department_df, to match specification
+    - columns "created_at" and "last_updated" are dropped from staff_df, to match specification
+    - staff_df is left merged with department_df on depatment_id, to add location data for each staff member
+    - column, "department_id" is dropped from staff_df, to match specification
+    - transformed staff_df (dataframe) is returned
+
+    Arguments: no arguments.
+
+    Returns: nothing (if no new staff data), or a dataframe containing new, transformed staff data.
+
+    """
+
     most_recent_file = find_most_recent_json_filename("staff", "fscifa-raw-data")
     if most_recent_file:
         staff_df = json_to_pd_dataframe(most_recent_file, "staff", "fscifa-raw-data")
     department_df = get_department_data()
-
     department_df.drop(
         ["manager", "created_at", "last_updated"],
         axis=1,
         inplace=True,
     )
-
     staff_df.drop(
         ["created_at", "last_updated"],
         axis=1,
         inplace=True,
     )
-
     merge_staff_to_department_df = pd.merge(
         staff_df, department_df, on="department_id", how="left"
     )
-
     merge_staff_to_department_df.drop(
         ["department_id"],
         axis=1,
         inplace=True,
     )
-
     return merge_staff_to_department_df
 
 
 def transform_dim_design():
+    """
+    This function:
+    - calls find_most_recent_json_filename
+    - check whether this returns a file name string (which will be the case if new data has been added to the design table in the totesys database)
+    - if an exception is raised, transform_dim_design returns nothing (because there is no new data to be transformed)
+    - otherwise, json_to_pd_dataframe is invoked, which returns a dataframe for new design data, design_df
+    - columns "created_at" and "last_updated" are dropped from design_df, to match specification
+    - transformed design_df (dataframe) is returned
+
+    Arguments: no arguments.
+
+    Returns: nothing (if no new design data), or a dataframe containing new, transformed design data.
+
+    """
     most_recent_file = find_most_recent_json_filename("design", "fscifa-raw-data")
     if most_recent_file:
         design_df = json_to_pd_dataframe(most_recent_file, "design", "fscifa-raw-data")
