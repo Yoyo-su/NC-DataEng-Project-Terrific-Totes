01d6cff6329a9eadeed73f7a9da6c3db7625d150
CharlotteC63 <ccampbell6066@gmail.com>
2025-06-05 14:12:21 +0100

    update docstrings

--- src/utils/parquet_to_sql.py
+++ src/utils/parquet_to_sql.py
@@ -6,9 +6,31 @@ import io
 
 
 def fetch_parquet(table_name, bucket):
-    
+    """
+    This function:
+    - invokes find_most_recent_filename, to find the most recent parquet file for a given table_name, in the specified s3 bucket
+    - gets the s3 file path for the parquet file
+    - reads the parquet file, and puts its contents into a dataframe
+    - returns this dataframe
+
+    Parameters
+    ----------
+    table_name : str
+        Name of the dimensions or fact table
+    bucket : str
+        Name of the target S3 bucket.
+
+    Returns
+    ----------
+    A dataframe containing the contents of the latest parquet file for a given dimensions or fact table.
+
+    Raises
+    ----------
+    An Exception if any error was encountered when trying to read the parquet file from the s3 bucket, and return it as a dataframe.
+
+    """
+
     try:
-        # get most recent parquet from bucket with table name in filename
         most_recent_file = find_most_recent_filename(table_name, bucket, "parquet")
         s3 = boto3.client("s3")
         s3_file_path = f"{table_name}/{most_recent_file}"
@@ -21,12 +43,40 @@ def fetch_parquet(table_name, bucket):
 
 
 def parquet_to_sql(table_name, df):
+    """
+    This function:
+    - takes a dataframe for a given dimension or fact table
+    - puts the columns in that dataframe into a list
+    - puts the rows in that dataframe into a list
+    - constructs a SQL query that will insert values into a given dimension or fact table
+    - connects to the data warehouse (a postgres database)
+    - runs the query within the database
+    - returns a success message to confirm successful insertion into the data warehouse
+
+    Parameters
+    ----------
+    table_name : str
+        Name of a dimension or fact table
+    df : dataframe
+        Dataframe of the dimension or fact table, table_name
+
+    Returns
+    ----------
+    A success message to confirm successful insertion into the data warehouse.
+
+    Raises
+    ----------
+    An Exception if any error was encountered when trying to insert into the data warehouse.
+
+    """
     try:
-        # make parquetfile a dataframe and seperate columns/values
+        # put dataframe column names into a list:
         columns = df.columns.tolist()
+
+        # put dataframe values into a list:
         values = df.values.tolist()
 
-        # construct sql query
+        # construct SQL query
         query = f"INSERT INTO {table_name} ("
         for column in columns:
             query += f"{column}, "
@@ -36,6 +86,8 @@ def parquet_to_sql(table_name, df):
                 query += f"{value}, "
             query = query[:-2] + "), ("
         query = query[:-3] + ";"
+
+        # connect to the data warehouse, and run the query
         conn = connect_to_db()
         conn.run(query)
         return {"result": "success"}
--- src/utils/upload_parquet_to_processed_zone.py
+++ src/utils/upload_parquet_to_processed_zone.py
@@ -1,29 +1,30 @@
-"""
-Uploads a Parquet file to a specified location in an S3 bucket.
-
-Parameters
-----------
-parquet_file : str
-    The local path to the Parquet file to be uploaded.
-bucket_name : str
-    The name of the target S3 bucket.
-key : str
-    The S3 key (path) where the file should be uploaded.
-s3_client : boto3.client
-    A Boto3 S3 client object used to perform the upload.
-
-Returns
--------
-str
-    A success message indicating the uploaded file path in S3.
-
-Raises
-------
-Exception
-    Propagates any exception raised during the upload process."""
-
-
 def upload_parquet_to_processed_zone(parquet_file, bucket_name, key, s3_client):
+    """
+    This function uploads a Parquet file to a specified location in an S3 bucket.
+
+    Parameters
+    ----------
+    parquet_file : str
+        The local path to the Parquet file to be uploaded.
+    bucket_name : str
+        The name of the target S3 bucket.
+    key : str
+        The S3 key (path) where the file should be uploaded.
+    s3_client : boto3.client
+        A Boto3 S3 client object used to perform the upload.
+
+    Returns
+    -------
+    str
+        A success message indicating the uploaded file path in S3.
+
+    Raises
+    ------
+    Exception
+    Propagates any exception raised during the upload process.
+    
+    """
+
     try:
         s3_client.upload_file(parquet_file, bucket_name, key)
         print(f"Successfully uploaded {key} to s3://{bucket_name}/{key}")
