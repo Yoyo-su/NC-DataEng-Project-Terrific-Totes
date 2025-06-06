1ababaf035b339102bcb62c2fb429cc700a2ac2f
sukansasi <sukanyasasi2013@gmail.com>
2025-06-06 16:26:49 +0100

    Added return value to find_most_recent_filename

--- src/python/utils/find_most_recent_filename.py
+++ src/python/utils/find_most_recent_filename.py
@@ -74,6 +74,7 @@ def find_most_recent_file(files, table_name, bucket_name, filetype="json"):
             return most_recent_file
         else:
             print(f"No new data for table, {table_name}")
+            return None
     except IndexError:
         raise IndexError(
             f"No file containing table, {table_name} is found in the s3 bucket"
