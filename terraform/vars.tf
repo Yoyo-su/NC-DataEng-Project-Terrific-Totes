d1de6f5cd782f567dc19a66e46f44b7f9e40f5b8
Yoyo-su <iohannan211@gmail.com>
2025-06-06 10:45:25 +0100

    Include warehouse credentials

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -23,20 +23,28 @@ variable "python_runtime" {
   default = "python3.13"
 }
 
-
+# shared db variables
 variable "pg_user" {
-  sensitive = true
-}
-
-variable "pg_password" {
   sensitive = true
 }
 
 variable "pg_port" {
 }
 
-
+# totesys db variables
 variable "pg_host" {}
 
-
 variable "pg_database" {}
+
+variable "pg_password" {
+  sensitive = true
+}
+
+# data warehouse variables
+variable "dw_host" {}
+
+variable "dw_database" {}
+
+variable "dw_password" {
+  sensitive = true
+}
