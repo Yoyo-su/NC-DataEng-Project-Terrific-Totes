8d8363aa0dc30f125358ebd7380ef6c4f445d4f2
Yoyo-su <iohannan211@gmail.com>
2025-06-09 11:26:30 +0100

    Fix string handling in sql query

--- tests/test_parquet_to_sql.py
+++ tests/test_parquet_to_sql.py
@@ -109,5 +109,5 @@ class TestParquetToSQL:
         parquet_to_sql(table_name, test_df)
         assert mock_conn.run.called  # Ensure .run() was called
         mock_conn.run.assert_called_with(
-            "INSERT INTO dim_staff (sales_order_id, staff, counterparty_id, currency_id) VALUES(7, hello, 420, 1), (1, wo''rld, 13, 3);"
+            "INSERT INTO dim_staff (sales_order_id, staff, counterparty_id, currency_id) VALUES(7, \"hello\", 420, 1), (1, \"wo''rld\", 13, 3);"
         )  # Ensure expected query was run
